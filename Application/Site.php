dc48bb4fffaf3eb658ae21eb4387028e9e27b971
edioilha <edioilha@gmail.com>
2015-02-25 23:03:38 -0300

    Initial classes

---
+++ Application/Site.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: edio
+ * Date: 2/25/15
+ * Time: 10:56 PM
+ */
+
+class Site {
+
+    private $name;
+
+    public function initialize() {
+        $this->name = "Name of Site";
+    }
+}
