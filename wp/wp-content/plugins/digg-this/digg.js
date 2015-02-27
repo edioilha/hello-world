558bf73ea15450a2c7ed2fe3d8ac85847e57f68c
Edio Ilha <edioilha@gmail.com>
2015-02-27 17:57:53 -0300

    Updated WP-Plugin files

--- wp/wp-content/plugins/digg-this/digg.js
+++ wp/wp-content/plugins/digg-this/digg.js
@@ -1,3 +1,14 @@
 /**
  * Created by edio on 2/27/15.
  */
+
+(function() {
+    var s = document.createElement('SCRIPT'), s1 = document.
+        getElementsByTagName('SCRIPT')[0];
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'http://widgets.digg.com/buttons.js';
+    s1.parentNode.insertBefore(s, s1);
+})();
+
+/*End of File*
--- wp/wp-content/plugins/digg-this/index.php
+++ wp/wp-content/plugins/digg-this/index.php
@@ -4,4 +4,40 @@
  * User: edio
  * Date: 2/27/15
  * Time: 1:53 PM
- */ 
+ */
+
+/* Information Header Goes here */
+
+/*
+Plugin Name: Digg This
+Plugin URI: http://wordpress.org/
+Description: Digg Plugin
+Author: Edio Ilha
+Version: 1.0
+Author URI: http://wordpress.org
+*/
+
+// include() or require() any necessary files here...
+// Settings and/or Configuration Details go here...
+// Tie into WordPress Hooks and any functions that should run on load.
+// "Private" internal functions named with a leading underscore
+
+function _diggthis_get_post_description() { }
+function _diggthis_get_post_media_type() { }
+function _diggthis_get_post_title() { }
+function _diggthis_get_post_topic() { }
+function _diggthis_get_post_url() { }
+
+// The "Public" functions
+function diggthis_add_js_to_doc_head() {
+    echo $src = plugins_url('digg.js',__FILE__);
+    //wp_register_script('diggthis', $src);
+    //wp_enqueue_script ('diggthis');
+}
+add_action('init','diggthis_add_js_to_doc_head');
+
+function diggthis_check_wordpress_version() { }
+function diggthis_get_button() { }
+
+
+/* EOF */
