d1a465e565219e81a497036d54b4df09b78b1d1e
edioilha <edioilha@gmail.com>
2015-03-10 21:46:01 -0300

    Src updated WP

---
+++ wp/test1.php
@@ -0,0 +1,2 @@
+   header('Content-type: text/plain');
+   print " -------- I think I'm getting a clue!";
---
+++ wp/wp-content/plugins/Twitter/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Twitter
+ * @version 1.0
+ */
+/*
+Plugin Name: Twitter
+Plugin URI: http://wordpress.org
+Description: Twitter
+Author: Edio Ilha
+Version: 1.0.0
+Author URI: http://wordpress.org
+*/
+
